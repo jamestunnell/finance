ac4051897116979563658740e156fff4b012ccaf
James Tunnell <jamestunnell@lavabit.com>
2013-08-20 16:53:13 -0700

    Remove overall budget amount, since this can just be calculated from adding the allocations. Rename Budget#total_allocation to #total. Remove #net_flow, #balanced?, #surplus?, and #deficit?. Allow an allocation value to be a Budget object. Total is calculated recursively. Added an ASCII table output via #to_s, using the 'terminal-table' gem.

--- lib/finance/planning/budget.rb
+++ lib/finance/planning/budget.rb
@@ -1,42 +1,39 @@
+require 'terminal-table'
+
 module Finance
 # Allocate an amount of some resource for various purposes
 class Budget
   class AmountNotPositiveError < StandardError; end
   
-  attr_reader :start_amount, :allocations
+  attr_reader :allocations
 
-  def initialize start_amount, allocations = {}
-    raise AmountNotPositiveError if start_amount <= 0
-    @start_amount = start_amount
-
-    allocations.each do |purpose,amount|
-      raise AmountNotPositiveError if amount <= 0
+  def initialize allocations = {}
+    allocations.each do |purpose,item|
+      unless item.is_a? Budget
+        raise AmountNotPositiveError if item <= 0
+      end
     end
     @allocations = allocations
   end
 
-  def total_allocation
+  def total
     total = 0
-    allocations.each do |purpose,amount|
-      total += amount
+    allocations.each do |purpose,item|
+      if item.is_a? Budget
+        total += item.total
+      else
+        total += item
+      end
     end
     return total
   end
 
-  def net_flow
-    return @start_amount - total_allocation
-  end
-
-  def balanced?
-    net_flow == 0
-  end
-
-  def surplus?
-    net_flow > 0
-  end
-
-  def deficit?
-    net_flow < 0
+  def to_s title = ""
+    args = { :headings => ['Item','Amount'], :rows => allocations.to_a }
+    unless title.empty?
+      args[:title] = title
+    end
+    Terminal::Table.new(args).to_s
   end
 end
 end
--- spec/planning/budget_spec.rb
+++ spec/planning/budget_spec.rb
@@ -1,86 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Budget do
-  before :all do
-    @allocations = {
-      {:groceries => 250, :utilities => 300, :rent => 800} => 1350,
-      {"R & D" => 1200000, "QA" => 400000, "Sales & Marketing" => 300000, "Facilities" => 80000} => 1980000,
-    }
-  end
+  describe '#total' do
+    context 'only numeric objects given for allocation values' do
+      it 'should return the sum of the numeric objects' do
+        Budget.new(:a => 25, :b => 32.5, :c => 1200).total.should eq(1257.5)
+      end
+    end
 
-  describe '#total_allocation' do
-    it 'should sum all the allocation amounts' do
-      @allocations.each do |allocations_hash, expected_total|
-        Budget.new(1, allocations_hash).total_allocation.should eq(expected_total)
+    context 'only Budget objects given for allocation values' do
+      it 'should return the sum of all the Budget objects\' totals' do
+        a = Budget.new(:a => 10, :b => 20)
+        b = Budget.new(:a => 25, :b => 33)
+        Budget.new(:a => a, :b => b).total.should eq(88)
+      end
+    end
+
+    context 'mix of numeric and Budget objects given for allocation values' do
+      it 'should return the sum of all the numeric objects and the Budget objects\' totals' do
+        a = Budget.new(:a => 10, :b => 20)
+        b = Budget.new(:a => 25, :b => 33)
+        Budget.new(:a => a, :b => b, :c => 100, :d => 2).total.should eq(190)
       end
     end
   end
-
-  describe '#net_flow' do
-    it 'should return the difference between start amount and total allocation' do
-      [ 500,-1200,0,105030].each do |expected_net_flow|
-        @allocations.each do |allocations_hash, expected_total|
-          Budget.new(expected_total + expected_net_flow, allocations_hash).net_flow.should eq(expected_net_flow)
-        end
-      end
-    end
-  end
-
-  describe '#balanced?' do
-    context 'net_flow is 0' do
-      it 'should return true' do
-        @allocations.each do |allocations_hash, expected_total|
-          Budget.new(expected_total, allocations_hash).balanced?.should be_true
-        end
-      end
-    end
-
-    context 'net_flow is not 0' do
-      it 'should return false' do
-        @allocations.each do |allocations_hash, expected_total|
-          Budget.new(expected_total + rand(1..10), allocations_hash).balanced?.should be_false
-          Budget.new(expected_total - rand(1..10), allocations_hash).balanced?.should be_false
-        end
-      end
-    end
-  end
-
-  describe '#surplus?' do
-    context 'net_flow is > 0' do
-      it 'should return true' do
-        @allocations.each do |allocations_hash, expected_total|
-          Budget.new(expected_total + rand(1..10), allocations_hash).surplus?.should be_true
-        end
-      end
-    end
-
-    context 'net_flow is <= 0' do
-      it 'should return false' do
-        @allocations.each do |allocations_hash, expected_total|
-          Budget.new(expected_total, allocations_hash).surplus?.should be_false
-          Budget.new(expected_total - rand(1..10), allocations_hash).surplus?.should be_false
-        end
-      end
-    end
-  end
-
-  describe '#deficit?' do
-    context 'net_flow is < 0' do
-      it 'should return true' do
-        @allocations.each do |allocations_hash, expected_total|
-          Budget.new(expected_total - rand(1..10), allocations_hash).deficit?.should be_true
-        end
-      end
-    end
-
-    context 'net_flow is >= 0' do
-      it 'should return false' do
-        @allocations.each do |allocations_hash, expected_total|
-          Budget.new(expected_total, allocations_hash).deficit?.should be_false
-          Budget.new(expected_total + rand(10), allocations_hash).deficit?.should be_false
-        end
-      end
-    end
-  end
-
 end
